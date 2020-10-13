ebb9d4d9aa8e4c0b816e6bb68be4f05d34e1c343
Liam Wang <pcnubhee@gmail.com>
2020-10-13 09:59:25 +0800

    project in

---
+++ README.md
@@ -0,0 +1,59 @@
+# FoodHelper 你的熱門餐廳
+
+FoodHelper connects people with great local restaurants.
+![Restaurant](./Restaurant screenshot_main page)
+![Restaurant](./Restaurant screenshot_edit page)
+
+## 產品功能
+
+- 使用者可以瀏覽每一間餐廳的詳細資訊
+- 提供使用者瀏覽和搜尋餐廳的功能
+- 使用者可以修改一間餐廳的資訊
+- 使用者可以新增一間餐廳的資訊
+- 用者可以刪除一間餐廳
+
+## 使用套件及安裝環境
+
+- 開發環境：[VS code](https://visualstudio.microsoft.com/zh-hant/)
+- 後端語言：[Node.js](https://nodejs.org/en/)
+- 後端框架：[Express](https://expressjs.com/)
+- 資料庫：[MongoDB](https://www.mongodb.com/)
+- mongoose：[Mongoose](https://www.npmjs.com/package/mongoose)
+
+## 安裝及使用方式
+
+1. 在電腦開啟 terminal 輸入指令，Clone 專案下載專案資料夾中
+
+```
+git clone https://github.com/CH-W0423/restaurant_CRUD_A7.git
+```
+
+2. 開啟終端機 (Terminal)，輸入指令進入此專案資料夾
+
+```
+cd restaurant_CRUD_A7
+```
+
+3. 專案資料夾中使用，安裝套件 npm
+
+```
+$ npm install
+```
+
+4. MongoDB 產生預設使用者及餐廳資料 (匯入 Seed data)
+
+```
+npm run seed
+```
+
+5. 安裝 nodemon 套件，並啟動伺服器，執行程式
+
+```
+npm run dev
+```
+
+6. 至瀏覽器輸入本地網址
+
+```
+http://localhost:3000
+```
---
+++ app.js
@@ -0,0 +1,136 @@
+const express = require("express");
+const mongoose = require('mongoose')
+const exphbs = require("express-handlebars");
+const bodyParser = require ('body-parser')
+const Restaurant = require("./models/restaurant");
+const app = express();
+const port = 3000;
+
+// 連線資料庫(mongoDB)
+mongoose.connect("mongodb://localhost/restaurant-list", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}); 
+
+// 取得資料庫連線狀態
+const db = mongoose.connection;
+
+// 連線異常
+db.on("error", () => {
+  console.log("mongodb error!");
+});
+// 連線成功
+db.once("open", () => {
+  console.log("mongodb connected!");
+});
+
+// 設定引擎
+app.engine("hbs", exphbs({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs"); 
+//使用靜態檔
+app.use(express.static("public")); 
+// start and listen on the Express server
+app.listen(port, () => {
+  console.log(`Express is listening on http://localhost:${port}`);
+});
+
+// 設定首頁路由，查看全部資料
+app.get("/", (req, res) => {
+  Restaurant.find() 
+    .lean() 
+    .then((restaurants) => res.render("index", { restaurants })) 
+    .catch((error) => console.error(error)); 
+});
+
+// add new routes setting
+app.get("/restaurants/new", (req, res) => {
+  return res.render("new")
+})
+
+//在資料庫新增資料的路由
+app.post('/restaurants', (req, res) => {
+  console.log(req.body)
+  if(!req.body.image.length){
+    req.body.image='https://static.vecteezy.com/system/resources/previews/000/091/119/large_2x/free-restaurant-logo-on-paper-plate-vector.jpg'
+  }
+    const restaurant = req.body
+    return Restaurant.create(restaurant)
+      .then(() => res.redirect('/'))
+      .catch((error) => console.log(error))
+})
+
+//設動態路由，查看特定餐廳詳情
+app.get("/restaurants/:id", (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('edit', { restaurant }))
+    .catch((error) => console.log(error))
+})
+
+//前往編輯頁面路由
+app.get('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .lean()
+    .then((restaurant) => res.render('edit', { restaurant }))
+    .catch((error) => console.log(error))
+})
+
+//資料從表單回傳後，更新資料庫
+app.post('/restaurants/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  const nameEn = req.body.name_en
+  const category = req.body.category
+  const image = req.body.image
+  const location = req.body.location
+  const phone = req.body.phone
+  const googleMap = req.body.google_map
+  const rating = req.body.rating
+  const description = req.body.description
+  return restaurantList.findById(id)
+      //如果查詢成功，儲存資料
+      .then((restaurant) => {
+        restaurant.name = name
+        restaurant.nameEn = nameEn
+        restaurant.category = category
+        restaurant.image = image
+        restaurant.location = location
+        restaurant.phone = phone
+        restaurant.googleMap = googleMap
+        restaurant.rating = rating
+        restaurant.description = description
+        return restaurant.save()
+      })
+      //如果儲存成功，重新導向該筆的詳細頁面
+      .then(() => res.redirect(`/restaurants/${id}`))
+      .catch((error) => console.log(error))
+})
+
+
+//去資料庫刪除資料
+app.post('/restaurants/:id/delete', (req, res) => {
+  const id = req.params.id
+  return Restaurant.findById(id)
+    .then((restaurant) => restaurant.remove())
+    .then(() => res.redirect('/'))
+    .catch((error) => console.log(error))
+})
+
+//搜尋功能
+app.get('/search', (req, res) => {
+  console.log(req.query)
+  const keyword = req.query.keyword
+  Restaurant.find()
+    .lean()
+    .then(restaurants => {
+      return restaurants.filter(restaurant =>
+        restaurant.name.toLowerCase().includes(keyword)
+        || restaurant.name_en.toLowerCase().includes(keyword)
+        || restaurant.category.toLowerCase().includes(keyword)
+      )
+    })
+    .then(restaurants => res.render('index', { restaurants, keyword }))
+    .catch(error => console.log(error))
+})
